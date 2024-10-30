25e1c8db8f92bc074a18750f5ee12313c148e7b0
cdrmack <git.pynvt@passmail.net>
2024-10-30 14:36:50 +0100

    add editorconfig, update gitattributes

--- init.el
+++ init.el
@@ -1,3 +1,3 @@
 ;; find and load Emacs Lisp source code blocks in the `config.org` file
 (org-babel-load-file
- (expand-file-name "config.org" user-emacs-directory))
+  (expand-file-name "config.org" user-emacs-directory))
