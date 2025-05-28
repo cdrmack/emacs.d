2aebaead68c8e61ec6b1a0073aaed60fa995bd9d
cdrmack <git.pynvt@passmail.net>
2025-05-28 19:02:22 +0200

    enable lexical binding, display current function name

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;;; ...  -*- lexical-binding: t -*-
 ;; find and load Emacs Lisp source code blocks in the `config.org` file
 (org-babel-load-file
   (expand-file-name "config.org" user-emacs-directory))
